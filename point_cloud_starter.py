07109d3d8aecfba9cb6210d3e24a8d661df5dfa1
Taru Rustagi <taru0904@umd.edu>
2021-03-30 18:51:00 -0400

    Clustering and bounding boxes done

--- point_cloud_starter.py
+++ point_cloud_starter.py
@@ -1,49 +1,61 @@
-'''
-@article{Zhou2018,
-	author    = {Qian-Yi Zhou and Jaesik Park and Vladlen Koltun},
-	title     = {{Open3D}: {A} Modern Library for {3D} Data Processing},
-	journal   = {arXiv:1801.09847},
-	year      = {2018},
-}
-'''
-## IMPORT LIBRARIES
+
 import numpy as np
 import time
-import open3d
+import open3d as od
 import pandas as pd
 import matplotlib.pyplot as plt
 
-## USE http://www.open3d.org/docs/release/tutorial/Basic/
+## http://www.open3d.org/docs/release/tutorial/Basic/
+#Visualization of point cloud
+pcd = od.io.read_point_cloud("test_files/KITTI/000002.pcd")
+od.visualization.draw_geometries([pcd])
+print(np.asarray(pcd.points))
 
-## CHALLENGE 1 - OPEN A FILE OF YOUR CHOICE AND VISUALIZE THE POINT CLOUD
-# The supported extension names are: pcd, ply, xyz, xyzrgb, xyzn, pts.
-pcd = 
-
-## IF YOU HAVE PPTK INSTALLED, VISUALIZE USING PPTK
-#import pptk
-#v = pptk.viewer(pcd.points)
-
-## CHALLENGE 2 - VOXEL GRID DOWNSAMPLING
+# Voxel grid Downsampling
 print(f"Points before downsampling: {len(pcd.points)} ")
-pcd = 
+pcd = pcd.voxel_down_sample(voxel_size=0.05)
 print(f"Points after downsampling: {len(pcd.points)}")# DOWNSAMPLING
 
+#Ransac plane detection
+_, inliers = pcd.segment_plane(distance_threshold=0.25,
+                                         ransac_n=3,
+                                         num_iterations=1000)
+inlier_cloud = pcd.select_by_index(inliers)
+inlier_cloud.paint_uniform_color([1.0, 0, 0])
+outlier_cloud = pcd.select_by_index(inliers, invert=True)
+outlier_cloud.paint_uniform_color([0, 1.0, 0])
+od.visualization.draw_geometries([inlier_cloud, outlier_cloud])
 
-## CHALLENGE 3 - SEGMENTATION
-_, inliers = 
+# DBSCAN for object clustering
+pcd = outlier_cloud
+with od.utility.VerbosityContextManager(
+        od.utility.VerbosityLevel.Debug) as cm:
+    labels = np.array(
+        pcd.cluster_dbscan(eps=0.45, min_points=7, print_progress=True))
 
-## CHALLENGE 4 - CLUSTERING USING DBSCAN
-labels = 
-max_label= labels.max()
+max_label = labels.max()
 print(f"point cloud has {max_label + 1} clusters")
+colors = plt.get_cmap("tab20")(labels / (max_label if max_label > 0 else 1))
+colors[labels < 0] = 0
+pcd.colors = od.utility.Vector3dVector(colors[:, :3])
+od.visualization.draw_geometries([pcd])
 
-## BONUS CHALLENGE - CLUSTERING USING KDTREE AND KNN INSTEAD
-pcd_tree = 
+# Draw bounding boxes
+obbs = []
+indexes = pd.Series(range(len(labels))).groupby(labels,sort = False).apply(list).tolist()
+MAX_POINTS = 300
+MIN_POINTS = 40
 
-## CHALLENGE 5 - BOUNDING BOXES IN 3D
-bounding_boxes = 
+for i in range(0,len(indexes)):
+	nb_points = len(outlier_cloud.select_by_index(indexes[i]).points)
+	if nb_points > MIN_POINTS:
+		sub_cloud =outlier_cloud.select_by_index(indexes[i])
+		obb = sub_cloud.get_axis_aligned_bounding_box()
+		obb.color= (0,0,1)
+		obbs.append(obb)
 
-## CHALLENGE 6 - VISUALIZE THE FINAL RESULTS
-list_of_visuals = 
-
-## BONUS CHALLENGE 2 - MAKE IT WORK ON A VIDEO
+list_of_visuals = []
+list_of_visuals.append(outlier_cloud)
+list_of_visuals.extend(obbs)
+list_of_visuals.append(inlier_cloud)
+od.visualization.draw_geometries(list_of_visuals)
